cbe9a23fa8eb8899d3da5c7e5dac16b22285f29e
Daniele De Matteo <daniele@kuama.net>
2023-03-03 16:38:22 +0100

    feat: ensure stores are reset before rendering

--- src/main.js
+++ src/main.js
@@ -1,27 +1,33 @@
 import App from "./App.svelte";
-import { optionsStore, pollingStore, forcePollingStop } from "./store";
+import {optionsStore, pollingStore, forcePollingStop, paymentErrorsStore} from "./store";
 import {get} from "svelte/store";
 
 const renderApp = (selector, options) => {
-  // set client options to our store
-  optionsStore.set(options);
 
-  return new App({ target: selector });
+    // ensure stores are clean
+    pollingStore.set(undefined)
+    forcePollingStop.set(undefined)
+    paymentErrorsStore.set(undefined)
+
+    // set client options to our store
+    optionsStore.set(options);
+
+    return new App({target: selector});
 };
 
 const PayWithToonie = Object.freeze({
-  render: renderApp,
-  getStopPollingHandle: (stopPollingHandleReceiver) => {
-    const stopPolling = () => {
-      const currentTimeoutId = get(pollingStore);
-      if(currentTimeoutId) {
-        forcePollingStop.set(true);
-        clearTimeout(currentTimeoutId)
-      }
-    }
+    render: renderApp,
+    getStopPollingHandle: (stopPollingHandleReceiver) => {
+        const stopPolling = () => {
+            const currentTimeoutId = get(pollingStore);
+            if (currentTimeoutId) {
+                forcePollingStop.set(true);
+                clearTimeout(currentTimeoutId)
+            }
+        }
 
-    stopPollingHandleReceiver(stopPolling)
-  }
+        stopPollingHandleReceiver(stopPolling)
+    }
 });
 
 window.PayWithToonie = PayWithToonie;
