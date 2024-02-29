f26a59275d02ad18bf9ae10ffe257cffd5e24e55
Marco Polato <m.polato@kuama.net>
2024-02-29 10:58:06 +0100

    feat: implement Sentry log

--- src/main.js
+++ src/main.js
@@ -6,6 +6,26 @@ import {
   paymentErrorsStore,
 } from "./store";
 import { get } from "svelte/store";
+import * as Sentry from "@sentry/svelte";
+
+// Initialize the Sentry SDK here
+Sentry.init({
+  dsn: "https://0809360954ee4c8e8f695f634063221b@o508190.ingest.sentry.io/4506825020080128",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const renderApp = (selector, options) => {
   // ensure stores are clean
